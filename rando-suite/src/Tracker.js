7ec04abaad77575c2bec0db8ae724947b8088936
itwbaer <itwbaer@gmail.com>
2018-10-24 12:58:29 -0500

    Added sqlite 3, started some js functions

---
+++ rando-suite/src/Tracker.js
@@ -0,0 +1,19 @@
+//this is the component responsible for keeping track of and displaying items obtained.
+
+class Tracker extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	obtainables: 
+      
+    };
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Tracker;
---
+++ rando-suite/src/util.js
@@ -0,0 +1,10 @@
+function parseLogicalString(logicStr) {
+	// & means something is required
+	// | means it is one of a few options
+
+
+	//parse until we hit ) or & or |
+	//otherwise we will assume its a number aka an id
+
+	
+}
