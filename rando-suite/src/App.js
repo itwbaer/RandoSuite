3ed6085c1b5468ed3ba898655194b242cfd25ad2
itwbaer <itwbaer@gmail.com>
2018-10-24 20:37:59 -0500

    Grid Skeleton

--- rando-suite/src/App.js
+++ rando-suite/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container-fluid">
+        <div className="row" id="PrimaryRow">
+          <div className="grid-half col-4">
+            <div className="row" id="ChecklistNavRow">
+
+                Checklist Nav
+
+            </div>
+            <div className="row" id="ChecklistRow">
+ 
+                Checklist
+
+            </div>
+            <div className="row" id="TrackerRow">
+
+                Tracker
+
+            </div>
+          </div>
+          <div className="grid-half col-8">
+            <div className="row" id="MapNavRow">
+ 
+                MapNav
+
+            </div>
+            <div className="row" id="MapRow">
+
+                Map
+
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
