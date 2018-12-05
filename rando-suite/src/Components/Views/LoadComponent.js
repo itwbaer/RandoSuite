a8a2f5071f692e0bc4636fbd730278b7664cb37e
itwbaer <itwbaer@gmail.com>
2018-12-04 22:26:36 -0600

    Save/Load increase

--- rando-suite/src/Components/Views/LoadComponent.js
+++ rando-suite/src/Components/Views/LoadComponent.js
@@ -25,7 +25,7 @@ export class LoadComponent extends Component{
 		return(
 			<div className="container-fluid" id="">
 				<h5>Paste saved data below and click import to load</h5>	
-	   			<textarea className="form-control" key="load-box" rows="10" onChange={this.handleChange} defaultValue={this.state.loadFile}></textarea>
+	   			<textarea className="form-control" key="load-box" rows="15" onChange={this.handleChange} defaultValue={this.state.loadFile}></textarea>
 	   			<br/>
 	   			<button type="button" className="btn btn-light" onClick={this.handleClick}>Import</button>
 	   		</div>
